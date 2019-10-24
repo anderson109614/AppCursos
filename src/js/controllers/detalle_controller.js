2b0a4277f7e9a4b9792b88c3e327549b57f73816
anderson109614 <anderson109614@gmail.com>
2019-10-24 18:30:13 -0500

    jueves

--- src/js/controllers/detalle_controller.js
+++ src/js/controllers/detalle_controller.js
@@ -2,7 +2,7 @@ angular.module('AppCursos.controllers.Detalle', [])
 
 .controller('DetalleController', function($scope,$routeParams,$http){
   
-$http.get('http://localhost/ServicioPrueba/servicio.php?id='+$routeParams.cursoID).
+$http.get('http://10.7.8.226/ServicioPrueba/servicio.php?id='+$routeParams.cursoID).
 then(function(response) {
     $scope.curso = response.data;
 });
