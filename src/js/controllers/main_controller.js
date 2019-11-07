afb876f57c38744ad7e77a05d4e201814615fc32
anderson109614 <anderson109614@gmail.com>
2019-11-07 16:02:48 -0500

    guardato sin nada

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -5,7 +5,7 @@ angular.module('AppCursos.controllers.Main', [])
     ///
     desblo();
     ///
-    $http.get('http://10.7.8.226/ServicioPrueba/servicio.php').
+    $http.get('http://localhost/ServicioPrueba/servicio.php').
         then(function(response) {
             $scope.cursos = response.data;
     });
